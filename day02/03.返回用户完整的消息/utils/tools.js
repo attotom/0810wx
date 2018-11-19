70ad00ea40681c034d2ec0144711f81e613b0d80
WuJiaQi <WuJiaQi@example.com>
2018-11-19 09:03:54 +0800

    创建了新的分支,并更新为返回用户完整信息

---
+++ day02/03.返回用户完整的消息/utils/tools.js
@@ -0,0 +1,44 @@
+const {parseString} = require('xml2js');
+
+module.exports = {
+  getUserDataAsync (req) {
+    return new Promise(resolve => {
+
+      let result = '';
+      req
+        .on('data', data => {
+          console.log(data.toString()); //buffer
+          result += data.toString();
+        })
+        .on('end', () => {
+          console.log('用户数据接受完毕');
+          resolve(result);
+        })
+    })
+    
+  },
+  parseXMLDataAsync (xmlData) {
+    return new Promise((resolve, reject) => {
+      parseString(xmlData, {trim: true}, (err, data) => {
+        if (!err) {
+          resolve(data);
+        } else {
+          reject('parseXMLDataAsync方法出了问题：' + err);
+        }
+      })
+    })
+  },
+  formatMessage ({xml}) {
+
+    let result = {};
+
+    for (let key in xml) {
+
+      let value = xml[key];
+
+      result[key] = value[0];
+    }
+    
+    return result;
+  }
+}
